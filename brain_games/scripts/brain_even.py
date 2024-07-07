5d91d22c182bbdb7999b1909b4ad5e85955b9ade
Nikolaykrasnov <crasnov.colya2000@yandex.ru>
2024-07-07 19:20:34 +0500

    up brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -20,7 +20,7 @@ def main():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
     rounds_count = 3
     for _ in range(rounds_count):
